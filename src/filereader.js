498ffcb49930a22f487db868fca6bf889e34df3a
WalmartWarchief <juho.ylisir@gmail.com>
2023-03-19 18:36:47 +0200

    Added buttons

---
+++ src/filereader.js
@@ -0,0 +1,12 @@
+function showSelectedFile(){
+    selectedfile.value= document.getElementById("inputfile").value; 
+}
+document.getElementById('inputfile')
+        .addEventListener('change', function() {
+        var fr=new FileReader();
+        fr.onload=function(){
+            document.getElementById('output')
+                    .textContent=fr.result;
+        }
+        fr.readAsText(this.files[0]);
+        })
